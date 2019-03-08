d5221a48abf90b79f79f57736aaa5cb7d9cbc818
Mark Crowder <crowdered@gmail.com>
2019-03-08 16:43:35 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -15,7 +15,7 @@ def first_challenge
 
  contact["Freddy Mercury"].each do |attribute, value|
   if attribute == :favorite_icecream_flavors
-value.delete_if 
+value.delete_if do 
 
   #remember to return your newly altered contacts hash!
   contacts
